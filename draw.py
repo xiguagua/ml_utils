9abd627f19e6d2fd0666e140bbee9db85f8a8cac
rootxigua <rootxigua@outlook.com>
2020-02-15 21:54:39 +0800

    Func: plot_roc

--- draw.py
+++ draw.py
@@ -42,3 +42,18 @@ def plotImp(model, X, num = 30, lib='catboost'):
     ax.set_title("Catboost Feature Importance Ranking", fontsize = 14)
     ax.set_xlabel('')
     plt.show()
+
+def plot_roc(y_true, y_score_prob):
+    fpr, tpr, _ = metrics.roc_curve(y_true, y_score_prob)
+    roc_auc = metrics.auc(fpr, tpr)
+
+    plt.figure(figsize=(15, 10))
+    plt.title('Receiver Operating Characteristic')
+    plt.plot(fpr, tpr, 'b', label = 'AUC = %0.2f' % roc_auc)
+    plt.legend(loc = 'lower right')
+    plt.plot([0, 1], [0, 1],'r--')
+    plt.xlim([0, 1])
+    plt.ylim([0, 1])
+    plt.ylabel('True Positive Rate')
+    plt.xlabel('False Positive Rate')
+    plt.show()
